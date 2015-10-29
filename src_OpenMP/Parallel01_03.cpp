f8ca4e3701652771ccd4abbdb6d5073d2515d4f0
aditi-rajawat <aditi.rajawat@sjsu.edu>
2015-10-28 18:38:55 -0700

    Update Parallel01_03.cpp
    Edited paths for input files.

--- src_OpenMP/Parallel01_03.cpp
+++ src_OpenMP/Parallel01_03.cpp
@@ -70,7 +70,7 @@ int main(){
 					cout<<"Please enter the station Id"<< endl;
 					cin>> stationId;
 
-					string fileListpath = "/Users/sandyarathidas/Documents/CMPE275_Sandy/Project1/mesonet1/files.txt";
+					string fileListpath= "../Data/fileList.txt";
 					vector<string> vectorOfFilePaths = readFileList(fileListpath);
 
 					MesoData inputData = {MAX_NUM_DATA_POINTS,
@@ -167,7 +167,7 @@ void readData(MesoData & inputData, vector<string> List, string stationId) {
 
 
 	string line;
-	string path = "/Users/sandyarathidas/Documents/CMPE275_Sandy/Project1/mesonet1/";
+	string path = "../Data/";
 
 	omp_set_num_threads(NUM_OF_THREADS);
 
